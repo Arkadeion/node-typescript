29180c37592b18ec44293677bce5cbbcc48c6f1c
Arkadeion <arkadeion@gmail.com>
2022-09-16 14:15:17 +0200

    Integrate Prisma client with mocking

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client";
 
 const app = express();
 
-app.get("/planets", (request, response) => {
-    response.json([
-        { name: "Mercury" },
-        { name: "Venus" },
-        { name: "Jupiter" },
-    ]);
+app.get("/planets", async (request, response) => {
+    const planets = await prisma.planet.findMany();
+
+    response.json(planets);
 });
 
 export default app;
